1daeeec01e7d57b018bba760c45d0a5406a906d2
João Pedro Satelis <134563165+joaosatelis@users.noreply.github.com>
2025-05-10 22:19:30 -0300

    Update Atendimento.cs

--- ProjetoPetMedDigital/ProjetoPetMedDigital/Models/Atendimento.cs
+++ ProjetoPetMedDigital/ProjetoPetMedDigital/Models/Atendimento.cs
@@ -1,18 +1,2 @@
-﻿using PetMed_Digital.Models;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace ProjetoPetMedDigital.Models
-{
-    [Table("Atendimento")]
-    public class Atendimento : BaseModel
-    {
-        public Atendimento()
-        {
-            CreatedAt = DateTime.Now;
-            StatusAtendimento = "Pendente";
-        }
-
-        public string DoutorEscalado { get; set; }
-        public string StatusAtendimento { get; set; }
-    }
+﻿
 }
