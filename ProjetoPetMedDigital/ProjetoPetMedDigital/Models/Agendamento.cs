1e203a6b627d4224341c303d5541187c7fda6ff6
AnthonySSoares <anthonysilvasoares14@gmail.com>
2025-05-12 19:28:04 -0300

    Update Agendamento.cs

--- ProjetoPetMedDigital/ProjetoPetMedDigital/Models/Agendamento.cs
+++ ProjetoPetMedDigital/ProjetoPetMedDigital/Models/Agendamento.cs
@@ -14,6 +14,6 @@ namespace ProjetoPetMedDigital.Models
 		public DateTime DataAgendamento { get; set; }
 		public DateTime HoraAgendamento { get; set; }
 		public string Observacoes { get; set; }
-		public int IdValor { get; set; }
+		
 	}
 }
